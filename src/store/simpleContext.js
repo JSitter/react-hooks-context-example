8a79aebe86caad0fbd3ad4d28e56f8aa66f3258d
Justin Sitter <justin@jaytria.com>
2019-08-13 20:01:15 -0700

    add components and store

---
+++ src/store/simpleContext.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+// First we need a new Context
+export const CounterContext = React.createContext();
+
+// Next we need a provider
+export class CounterProvider extends Component {
+  state = {
+    counter: 0
+  }
+  render() {
+    return (
+      <CounterContext.Provider value={{
+        state: this.state,
+        addCount: () => this.setState({
+          counter: this.state.counter + 1
+        }),
+        subCount: () => this.setState({
+          counter: this.state.counter - 2
+        })
+      }}>
+        {this.props.children}
+      </CounterContext.Provider>
+    )
+  }
+}
